0cf861c3ac0c54ee20b23d67cc0556d0cd82a1df
Ademilton Mariano <ademilton.mariano@gmail.com>
2024-07-17 18:11:50 -0300

    primeira controller

---
+++ SistemaVendasCurso/Controllers/DepartamentoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVendasCurso.Models;
+
+namespace SistemaVendasCurso.Controllers;
+
+public class DepartamentoController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Departamento> list = new()
+        {
+            new Departamento { Id = 1, Nome = "Eletrônicos" },
+            new Departamento { Id = 2, Nome = "Alimentos" },
+            new Departamento { Id = 3, Nome = "Moda" }
+        };
+        return View(list);
+    }
+}
---
+++ SistemaVendasCurso/Models/Departamento.cs
@@ -0,0 +1,7 @@
+﻿namespace SistemaVendasCurso.Models;
+
+public class Departamento
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
